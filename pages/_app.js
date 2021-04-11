eb60adeb6f665753851099d96f1c8595dd044495
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-04-12 00:19:47 +0800

    Add Nproggress

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import { GeistProvider, CssBaseline } from "@geist-ui/react";
-import { useColorMode } from "react-use-color-mode";
-import { useEffect } from 'react';
+import NProgress from '../template/progress'
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <GeistProvider themeType={"dark"}>
+      <NProgress />
       <CssBaseline />
       <Component {...pageProps} />
     </GeistProvider>
